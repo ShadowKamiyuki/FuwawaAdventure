21865c4ffa3842b6fd5f942434f764c2d4a53b85
Pablo <171745324+ShadowKamiyuki@users.noreply.github.com>
2026-07-10 16:26:32 -0300

    update
    removed files and applied fixes for canvas and meshes in the game scene

--- Assets/_Scripts/Features/MainMenu/MainMenuView.cs
+++ Assets/_Scripts/Features/MainMenu/MainMenuView.cs
@@ -5,19 +5,16 @@ using System;
 public class MainMenuView : MonoBehaviour
 {
     [SerializeField] private Button startButton;
-    //[SerializeField] private Button optionsButton;
     [SerializeField] private Button quitButton;
 
     [SerializeField] private CanvasGroup canvasGroup;
 
     public event Action OnStartClicked;
-    //public event Action OnOptionsClicked;
     public event Action OnQuitClicked;
 
     private void Awake()
     {
         startButton.onClick.AddListener(() => OnStartClicked?.Invoke());
-        //optionsButton.onClick.AddListener(() => OnOptionsClicked?.Invoke());
         quitButton.onClick.AddListener(() => OnQuitClicked?.Invoke());
     }
 
